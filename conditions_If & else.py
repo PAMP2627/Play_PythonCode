25f816afe124243a68795ec5311bc079a758905d
PAMP2627 <42628302+PAMP2627@users.noreply.github.com>
2018-08-23 11:56:48 +0700

    Add files via upload

---
+++ conditions_If & else.py
@@ -0,0 +1,8 @@
+a = 10 # umur si a
+print("si a berumur :",a,"tahun") # menampilkan keterangan umur si a
+
+# membuat sebuah kondisi,dimana jika si a sudah cukup umur,maka si a boleh memilih pemimpin indonesia
+if  a < 17 :
+    print("si a belum cukup umur untuk memilih pemimpin untuk indonesia")
+else:
+    print("si a sudah cukup umur untuk memilih pemimpin indonesia")
