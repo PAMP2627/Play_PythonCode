42febb50d66f275fa41599334ce30ac6ca8c3ee8
PAMP2627 <42628302+PAMP2627@users.noreply.github.com>
2018-08-23 11:02:23 +0700

    Add files via upload

---
+++ Type_Data.py
@@ -0,0 +1,15 @@
+a = 1   # tipe data integer,menampung bilangan bulat
+b = 1.0 # tipe data float/double,menampung bilngan pecahan
+c = 'c' # tipe data char,menampung satu karakter huruf saja
+d = "hai kalian semuanya" # tipe data string,menampung lebih dari satu karakter huruf maupun angka
+
+# tipe data boolean,hanya memiliki 2 nilai yang pasti.yaitu nilai 1 dan 0 atau True dan False
+e = True
+f = False
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
