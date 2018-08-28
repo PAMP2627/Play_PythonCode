b20e9b4fb2784d134fe7c1182bac2afd14ec50a9
PAMP2627 <42628302+PAMP2627@users.noreply.github.com>
2018-08-28 10:41:45 +0700

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,10 @@
+'''
+Nama : Mr.zone
+Profesi : None
+Alamat : Di Atas Bumi Dibawah Langit
+'''
+
+print("di atas yang digunakan untuk komentar multi-baris")
+#ini adalah komentar yang digunakan untuk satu baris
+print("di file ini ada komentar saya")
+
